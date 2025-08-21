94dcc329c4d17f833a96e16991afe3d481e86f1c
Zenith <q1095568627@163.com>
2025-08-22 00:42:42 +0800

    refactor: refactor the poole-FTP service using alova.

--- services/wishing-pim-server/src/api/methods/poole-ftp.ts
+++ services/wishing-pim-server/src/api/methods/poole-ftp.ts
@@ -19,18 +19,44 @@ type PooleFTPFile = {
   owner: number;
 };
 export const getFilesList = (path: string) => {
-  const method = poole_ftp_alova.Post<{ files: PooleFTPFile[] }>(
+  const method = poole_ftp_alova.Post(
     "/files/list",
+    { path },
     {
-      path,
+      // 当响应数据需要再调用transform转换，那就需要在转换函数参数中指定类型，然后它的返回值类型将会作为响应数据类型。
+      transform(rawData: { files: PooleFTPFile[] }, headers) {
+        return rawData.files.map((file) => {
+          return {
+            ...file,
+            doc_type: path.split("/").at(-1),
+            doc_full_path: [path, file.name].join("/"),
+          };
+        });
+      },
     }
   );
   return method;
 };
 
 export const getFilesDownloadUrl = (files: string[]) => {
-  const method = poole_ftp_alova.Post<{ link: string }>("/files/download", {
-    files,
-  });
+  const method = poole_ftp_alova.Post(
+    "/files/download",
+    { files },
+    {
+      transform(rawData: { link: string }, headers) {
+        let file_name =
+          files.length === 1
+            ? files[0].split("/").at(-1)
+            : `Poole-FTP Files Download - ${files[0]
+                .split("/")
+                .slice(0, -1)
+                .join("/")}.zip`;
+        return {
+          download_url: rawData.link,
+          file_name,
+        };
+      },
+    }
+  );
   return method;
 };
